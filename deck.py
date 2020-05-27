0b20f88dbe8e2bceb81c95b956f928f3578ceeed
Marco Cusano <mcusano@live.com>
2020-05-27 23:29:10 +0100

    hand ranks
    logic is good but needs fixing
    
    need to find an easy way to test it outside

--- deck.py
+++ deck.py
@@ -9,14 +9,57 @@ from collections import deque
 import inspect
 
 def HandRank(cards):
-	def sort_cards_by_rank(cards):
+	# A hand are the 5 best cards!
+	cards_per_hand = 5
+	def SortCardsByRank(cards):
 		rankdict = dict([(t[1], t[0]) for t in list(enumerate(RANKS()))])
 		cards.sort(key=lambda x: rankdict[x.rank], reverse=True)
 		return cards
-	return ('whoknows', sort_cards_by_rank(cards))
+	# Pairs, sets and quads are one rank hands, changing only the number of cards
+	def OneRankHand(cards, n):
+		# Kickers are the best cards left between the hand and the total 5
+		kickers = cards_per_hand - n
+		# Return highest pair + highest 3 cards left
+		highest_rankhand = False
+		for rank in [x.rank for x in SortCardsByRank(cards)]:
+			if sum(card.rank == rank for card in cards) == n: highest_rankhand = [card for card in cards if card.rank == rank]
+			if highest_rankhand: break
+		if highest_rankhand:
+			for card in highest_rankhand: cards.remove(card)
+			cards = SortCardsByRank(cards)[:kickers]
+			return highest_rankhand + cards
+		return []
+	def HighCard(cards):
+		# Return 5 highest cards
+		return SortCardsByRank(cards)[:cards_per_hand]
+	def Pair(cards):
+		return OneRankHand(cards, 2)
+	def TwoPair(cards):
+		# Kickers are the best cards left between the hand and the total 5
+		kickers = 1
+		pairs = []
+		for rank in [x.rank for x in SortCardsByRank(cards)]:
+			if sum(card.rank == rank for card in cards) == 2: pairs.append([card for card in cards if card.rank == rank])
+		if len(pairs) >=2:
+			for card in [card for pair in pairs[:2] for card in pair]: cards.remove(card)
+			kicker = SortCardsByRank(cards)[0]
+			return [card for pair in pairs[:2] for card in pair] + kicker
+		return []
+	def Set(cards):
+		return OneRankHand(cards, 3)
+	def Quads(cards):
+		return OneRankHand(cards, 4)
+	# Exec all hand functions, the first strongest we find is our hand
+	for hand in reversed(HANDS()):
+		try:
+			hand_cards = locals()[hand](cards)
+			if any(hand_cards): return (hand, hand_cards)
+		except:
+			pass
 
 def PrintCards(cards):
-	return [(card.rank, card.suit) for card in cards]
+	try: return [(card.rank, card.suit) for card in cards]
+	except: return False
 
 def PopulatePositions(players):
 	number_of_players = len(players)
@@ -86,6 +129,7 @@ def Action(options):
 
 def RANKS(): return [ "2", "3", "4", "5", "6", "7","8", "9", "T", "J", "Q", "K", "A" ]
 def SUITS(): return [ "Clubs", "Diamonds", "Hearts", "Spades" ]
+def HANDS(): return [ "HighCard", "Pair", "TwoPair", "Set", "Straight", "Flush", "FullHouse", "Quads", "StraighFlush", "RoyalFlush" ]
 
 class Card:
 	def __init__(self, rank, suit):
--- event.py
+++ event.py
@@ -1,38 +1,52 @@
 class GlobalWealth(object):
-    def __init__(self):
-        self._global_wealth = 10.0
-        self._observers = []
+	def __init__(self):
+		self._global_wealth = 10.0
+		self._observers = []
 
-    @property
-    def global_wealth(self):
-        return self._global_wealth
+	@property
+	def global_wealth(self):
+		return self._global_wealth
 
-    @global_wealth.setter
-    def global_wealth(self, value):
-        self._global_wealth = value
-        for callback in self._observers:
-            print('announcing change')
-            callback(self._global_wealth)
+	@global_wealth.setter
+	def global_wealth(self, value):
+		self._global_wealth = value
+		for callback in self._observers:
+			print('announcing change')
+			callback(self._global_wealth)
 
-    def bind_to(self, callback):
-        print('bound')
-        self._observers.append(callback)
+	def bind_to(self, callback):
+		print('bound')
+		self._observers.append(callback)
 
 
 class Person(object):
-    def __init__(self, data):
-        self.wealth = 1.0
-        self.data = data
-        self.data.bind_to(self.update_how_happy)
-        self.happiness = self.wealth / self.data.global_wealth
+	def __init__(self, data):
+		self.wealth = 1.0
+		self.data = data
+		self.data.bind_to(self.update_how_happy)
+		self.happiness = self.wealth / self.data.global_wealth
 
-    def update_how_happy(self, global_wealth):
-        self.happiness = self.wealth / global_wealth
+	def update_how_happy(self, global_wealth):
+		self.happiness = self.wealth / global_wealth
 
 
 if __name__ == '__main__':
-    data = GlobalWealth()
-    p = Person(data)
-    print(p.happiness)
-    data.global_wealth = 1.0
-    print(p.happiness)
+	data = GlobalWealth()
+	p = Person(data)
+	print(p.happiness)
+	data.global_wealth = 1.0
+	print(p.happiness)
+
+class A(object):
+
+	def m(self, p_value):
+		 print p_value
+
+	@property
+	def p(self):
+		return self._p 
+
+	@p.setter
+	def p(self, value)
+		self._p = value
+		self.m(value)
